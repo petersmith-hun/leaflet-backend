8e60fcd19f705e6f391649d979be84687a21493b
Péter Kovács <peter.kovacs0519@gmail.com>
2017-10-03 22:30:01 +0200

    LFLT-174 Tags and attachments are missing in response

--- web/src/main/java/hu/psprog/leaflet/web/rest/conversion/entry/EntryVOToEditEntryDataModelEntityConverter.java
+++ web/src/main/java/hu/psprog/leaflet/web/rest/conversion/entry/EntryVOToEditEntryDataModelEntityConverter.java
@@ -5,11 +5,14 @@ import hu.psprog.leaflet.api.rest.response.entry.EditEntryDataModel;
 import hu.psprog.leaflet.api.rest.response.user.UserDataModel;
 import hu.psprog.leaflet.service.vo.EntryVO;
 import hu.psprog.leaflet.web.rest.conversion.CommonFormatter;
+import hu.psprog.leaflet.web.rest.conversion.file.UploadedFileVOToFileDataModelConverter;
+import hu.psprog.leaflet.web.rest.conversion.tag.TagVOToTagDataModelEntityConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
 
 /**
  * Converts {@link EntryVO} value object to {@link EditEntryDataModel} model.
@@ -21,11 +24,17 @@ public class EntryVOToEditEntryDataModelEntityConverter implements Converter<Ent
 
     private CommonFormatter commonFormatter;
     private HttpServletRequest httpServletRequest;
+    private UploadedFileVOToFileDataModelConverter uploadedFileVOToFileDataModelConverter;
+    private TagVOToTagDataModelEntityConverter tagVOToTagDataModelEntityConverter;
 
     @Autowired
-    public EntryVOToEditEntryDataModelEntityConverter(CommonFormatter commonFormatter, HttpServletRequest httpServletRequest) {
+    public EntryVOToEditEntryDataModelEntityConverter(CommonFormatter commonFormatter, HttpServletRequest httpServletRequest,
+                                                      UploadedFileVOToFileDataModelConverter uploadedFileVOToFileDataModelConverter,
+                                                      TagVOToTagDataModelEntityConverter tagVOToTagDataModelEntityConverter) {
         this.commonFormatter = commonFormatter;
         this.httpServletRequest = httpServletRequest;
+        this.uploadedFileVOToFileDataModelConverter = uploadedFileVOToFileDataModelConverter;
+        this.tagVOToTagDataModelEntityConverter = tagVOToTagDataModelEntityConverter;
     }
 
     @Override
@@ -49,7 +58,13 @@ public class EntryVOToEditEntryDataModelEntityConverter implements Converter<Ent
                 .withUser(UserDataModel.getBuilder()
                         .withId(entryVO.getOwner().getId())
                         .withUsername(entryVO.getOwner().getUsername())
-                        .build());
+                        .build())
+                .withAttachments(entryVO.getAttachments().stream()
+                        .map(uploadedFileVOToFileDataModelConverter::convert)
+                        .collect(Collectors.toList()))
+                .withTags(entryVO.getTags().stream()
+                        .map(tagVOToTagDataModelEntityConverter::convert)
+                        .collect(Collectors.toList()));
 
         return builder.build();
     }
