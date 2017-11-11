b3ae382d1528708d9bf22c9446b1369768d41dd7
Péter Kovács <peter.kovacs0519@gmail.com>
2017-11-11 18:35:00 +0100

    LFLT-193 Deficient data for listing categories in LMS

--- web/src/main/java/hu/psprog/leaflet/web/rest/conversion/category/CategoryVOToCategoryDataModelEntityConverter.java
+++ web/src/main/java/hu/psprog/leaflet/web/rest/conversion/category/CategoryVOToCategoryDataModelEntityConverter.java
@@ -2,8 +2,12 @@ package hu.psprog.leaflet.web.rest.conversion.category;
 
 import hu.psprog.leaflet.api.rest.response.category.CategoryDataModel;
 import hu.psprog.leaflet.service.vo.CategoryVO;
+import hu.psprog.leaflet.web.rest.conversion.CommonFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Converts {@link CategoryVO} value object to {@link CategoryDataModel} model.
@@ -13,12 +17,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class CategoryVOToCategoryDataModelEntityConverter implements Converter<CategoryVO, CategoryDataModel> {
 
+    private CommonFormatter commonFormatter;
+    private HttpServletRequest httpServletRequest;
+
+    @Autowired
+    public CategoryVOToCategoryDataModelEntityConverter(CommonFormatter commonFormatter, HttpServletRequest httpServletRequest) {
+        this.commonFormatter = commonFormatter;
+        this.httpServletRequest = httpServletRequest;
+    }
+
     @Override
     public CategoryDataModel convert(CategoryVO source) {
 
         return CategoryDataModel.getBuilder()
                 .withID(source.getId())
                 .withTitle(source.getTitle())
+                .withDescription(source.getDescription())
+                .withCreated(commonFormatter.formatDate(source.getCreated(), httpServletRequest.getLocale()))
+                .withLastModified(commonFormatter.formatDate(source.getLastModified(), httpServletRequest.getLocale()))
+                .withEnabled(source.isEnabled())
                 .build();
     }
 }
