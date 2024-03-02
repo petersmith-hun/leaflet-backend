abab8e68845c9e9185d12b6574f13b159bfef2f0
Péter Kovács <peter.kovacs0519@gmail.com>
2024-03-02 12:10:28 +0100

    LFLT-603 Implement CORS support in Leaflet  - Added CORS configuration to security and MVC configurations  - Aligned ResponseFillerInterceptor to ignore OPTIONS requests  - Aligned related test  - Bumped application version to 2.3.1-dev

--- web/src/main/java/hu/psprog/leaflet/web/config/WebMVCConfiguration.java
+++ web/src/main/java/hu/psprog/leaflet/web/config/WebMVCConfiguration.java
@@ -1,10 +1,14 @@
 package hu.psprog.leaflet.web.config;
 
 import hu.psprog.leaflet.web.interceptor.ResponseFillerInterceptor;
+import lombok.AccessLevel;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,9 +18,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author Peter Smith
  */
 @Configuration
+@ConfigurationProperties(prefix = "cors")
+@Setter(AccessLevel.PACKAGE)
 public class WebMVCConfiguration implements WebMvcConfigurer {
 
     private final ResponseFillerInterceptor responseFillerInterceptor;
+    private boolean enableCors;
+    private String[] allowedOrigins;
 
     @Autowired
     public WebMVCConfiguration(ResponseFillerInterceptor responseFillerInterceptor) {
@@ -31,5 +39,15 @@ public class WebMVCConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(responseFillerInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        if (enableCors) {
+            registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins(allowedOrigins);
+        }
     }
 }
